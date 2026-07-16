98f420eb0ec77e5e563b5049ebcb0810e2632c5f
Rogerin81 <rogeric3081@gmail.com>
2026-07-16 13:55:25 -0400

    Pagina web completa

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Concejo Municipal de Potosí</title>
+    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
     @vite(['resources/js/app.js', 'resources/css/app.css'])
 </head>
 <body>
